b148be57e44de3cfead17575e3a61f1b8bb519c4
kodxxl <xsrc@mail.ru>
2024-10-18 10:16:52 +0400

    Some minor changes in source code formatting

--- sort/insertion_test.go
+++ sort/insertion_test.go
@@ -1,15 +1,15 @@
 package sort
 
-import(
-	"testing"
+import (
 	"reflect"
+	"testing"
 )
 
 func TestInsertion(t *testing.T) {
 	got := Insertion([]int{5, 4, 3, 2, 1, 0})
 	want := []int{0, 1, 2, 3, 4, 5}
-	
-	if ! reflect.DeepEqual(got, want) {
+
+	if !reflect.DeepEqual(got, want) {
 		t.Log("Got: ", got)
 		t.Log("Want: ", want)
 		t.Fatalf("Array did not sorted propertly")
