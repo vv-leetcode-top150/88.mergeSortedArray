ef885262959b6b8aa270402e2af4d4f6e981e1b0
kodxxl <xsrc@mail.ru>
2024-10-18 08:29:47 +0400

    Simple test for insertion()

---
+++ sort/insertion_test.go
@@ -0,0 +1,15 @@
+package sort
+
+import(
+	"testing"
+	"reflect"
+)
+
+func TestInsertion(t *testing.T) {
+	got := Insertion([]int{5, 4, 3, 2, 1, 0})
+	want := []int{0, 1, 2, 3, 4, 5}
+	
+	if ! reflect.DeepEqual(got, want) {
+		t.Fatalf("Array did not sorted propertly")
+	}
+}
